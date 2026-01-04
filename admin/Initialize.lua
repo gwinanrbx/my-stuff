bf54898125f7fa1e19d326b52d853d0ba2cbaed3
gwinanrbx <smalltorchofficial@gmail.com>
2026-01-04 16:55:46 +0000

    Create Initialize.lua

---
+++ admin/Initialize.lua
@@ -0,0 +1,27 @@
+local Server = {}
+
+local RS = game:GetService('ReplicatedStorage')
+local ADMIN_FOLDER = RS:WaitForChild('Admin')
+local DATA = ADMIN_FOLDER:WaitForChild('Data')
+
+local events = ADMIN_FOLDER:WaitForChild('Events')
+local client = events:WaitForChild('client')
+local initialize_event = client:WaitForChild('Initialize')
+
+--// require
+
+local ADMINS_MODULE = require(DATA:WaitForChild('Admins'))
+
+--// module
+
+function Server.Admins(player : Player)
+	--print('is admin')
+	return ADMINS_MODULE[player.UserId] -- is player admin?	
+end
+
+function Server.SendData(player : Player)
+	print('[LOADED SERVER] Succesfully sent data to client')
+	initialize_event:FireClient(player)
+end
+
+return Server
